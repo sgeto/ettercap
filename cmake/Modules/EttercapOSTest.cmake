4a23dbb9e360c769ab3fa7f1ceb3c1fc7d82d449
Ali Abdulkadir <autostart.ini@gmail.com>
2017-07-09 00:04:19 +0300

    Added additional MinGW and MSVC specific compile and linker flags + some cleanup
    - Replaced hardcoded -fPIC flag with $PIC variable.
    - All flags concerning windows have been moved to EttercapOSTest.cmake
    - Added some custom Find*.cmake modules for MSVC (work in progress)
    - OS_CYGWIN should be using ec_cygwin.c (right?!)

--- cmake/Modules/EttercapOSTest.cmake
+++ cmake/Modules/EttercapOSTest.cmake
@@ -38,10 +38,56 @@ TEST_BIG_ENDIAN(WORDS_BIGENDIAN)
 set(CC_VERSION ${CMAKE_C_COMPILER})
 
 if(OS_WINDOWS)
-   set(gid_t int)
-   set(uid_t int)
-   set(uint unsigned int)
-   if(ENABLE_GTK)
-   find_package(PkgConfig REQUIRED)
-   endif(ENABLE_GTK)
+  set(gid_t int)
+  set(uid_t int)
+  set(uint unsigned int)
+  
+  add_definitions(-DWIN32_LEAN_AND_MEAN)
+  # add_definitions(-D_WIN32_WINNT=0x0501)
+  
+  if(ENABLE_GTK)
+    find_package(PkgConfig REQUIRED)
+  endif(ENABLE_GTK)
+  # Additional compiler and linker flags for windows should be set here *only*.
+  if(MINGW)
+    if(CMAKE_BUILD_TYPE STREQUAL Release)
+      set(CMAKE_RC_FLAGS "-Iinclude -v" CACHE STRING "" FORCE)
+    elseif(CMAKE_BUILD_TYPE STREQUAL RelWithDebInfo)
+      set(CMAKE_RC_FLAGS "-Iinclude -v" CACHE STRING "" FORCE)
+    elseif(CMAKE_BUILD_TYPE STREQUAL Debug)
+      set(CMAKE_RC_FLAGS "-D_DEBUG -Iinclude -v" CACHE STRING "" FORCE)
+    endif(CMAKE_BUILD_TYPE STREQUAL Release)
+
+    set(CMAKE_C_STANDARD_LIBRARIES "-lkernel32 -luser32 -lgdi32 -lwinspool -lshell32 -lole32 -loleaut32 -luuid -lcomdlg32 -ladvapi32 -lws2_32" CACHE STRING "" FORCE)
+
+    set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--output-def, -Wl,${PROJECT_NAME}.def -Wl,--warn-common -Wl,--no-as-needed -Wl,--nxcompat -Wl,--dynamicbase" CACHE STRING "" FORCE)
+
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_CREATE_CONSOLE_EXE}" CACHE STRING "" FORCE)
+
+  elseif(MSVC)
+    add_definitions(-D_CRT_SECURE_NO_DEPRECATE)
+
+    set(CMAKE_C_FLAGS "/DWIN32 /D_WINDOWS /W3 /nologo /errorReport:none /GS-" CACHE STRING "" FORCE)
+
+    if(CMAKE_BUILD_TYPE STREQUAL Release)
+    set(CMAKE_RC_FLAGS "/DWIN32 /D__NOTMINGW__ /Iinclude /v" CACHE STRING "" FORCE)
+    elseif(CMAKE_BUILD_TYPE STREQUAL RelWithDebInfo)
+    set(CMAKE_RC_FLAGS "/DWIN32 /D__NOTMINGW__ /Iinclude /v" CACHE STRING "" FORCE)
+    elseif(CMAKE_BUILD_TYPE STREQUAL Debug)
+    set(CMAKE_RC_FLAGS "/DWIN32 /D_DEBUG /D__NOTMINGW__ /Iinclude /v" CACHE STRING "" FORCE)
+    endif(CMAKE_BUILD_TYPE STREQUAL Release)
+
+    set(CMAKE_C_STANDARD_LIBRARIES "kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib ws2_32.lib" CACHE STRING "" FORCE)
+
+    set(CMAKE_SHARED_LINKER_FLAGS "/machine:${MSVC_C_ARCHITECTURE_ID} /nologo /NXCOMPAT /DYNAMICBASE /DELAYLOAD:wpcap.dll /ERRORREPORT:NONE" CACHE STRING "" FORCE)
+
+    set(CMAKE_EXE_LINKER_FLAGS "/machine:${MSVC_C_ARCHITECTURE_ID} ${CMAKE_CREATE_CONSOLE_EXE}" CACHE STRING "" FORCE)
+
+    include(FindZLIB)
+    include(FindPthreads)
+    include(FindIconv)
+    include(FindCURL)
+    include(FindPCAP)
+    include(FindLIBNET)
+  endif(MSVC)
 endif(OS_WINDOWS)
